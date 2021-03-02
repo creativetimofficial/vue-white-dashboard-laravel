8c36e2a46ecb61a38b67ee29e8cb1e400075024f
Team UPDIVISION <team@updivision.com>
2021-03-02 18:11:25 +0200

    latest

--- vue-white-dashboard/docs/quick-start.md
+++ vue-white-dashboard/docs/quick-start.md
@@ -7,11 +7,11 @@ project.
 
 #### Steps to install
 
-1. Navigate to your Nuxt Argon Dashboard project folder: `cd your-nuxt-argon-dashbord-project`
+1. Navigate to your Vue White Dashboard project folder: `cd your-vue-white-dashbord-project`
 2. Install project dependencies: `npm install`
 3. Create a new .env file: `cp .env.example .env`
-4. `VUE_APP_BASE_URL` should contain the URL of your Nuxt Argon Dashboard Project (eg. http://localhost:8080/)
-5. `VUE_APII_API_BASE_URL` should contain the URL of your Laravel JSON:API Project. (eg. http://localhost:3000/api/v1)
+4. `VUE_APP_BASE_URL` should contain the URL of your Vue White Dashboard Project (eg. http://localhost:8080/)
+5. `VUE_APP_API_BASE_URL` should contain the URL of your Laravel JSON:API Project. (eg. http://localhost:3000/api/v1)
 6. Run `npm run dev` to start the application in a local development environment or `npm run build` to build release distributables.
 
 #### Vue White Dashboard
@@ -20,13 +20,13 @@ Vue White Dashboard is built as Vue plugin so you can simply import it and use i
 
 ```js
 import Vue from 'vue';
-import DashboardPlugin from '@/plugins/blackDashboard'
+import DashboardPlugin from '@/plugins/whiteDashboard'
 Vue.use(DashboardPlugin);
 ```
 
 #### Global Components
 
-Argon comes with an extensive set of custom Vue components. Some of them are globally instantiated so
+White comes with an extensive set of custom Vue components. Some of them are globally instantiated so
 it's easier to use them across the app without importing them each time.
 
 Here's the list of global components:
@@ -40,7 +40,7 @@ Here's the list of global components:
 #### Local components
 
 Besides the components mentioned above, we have some extra components/plugins that are usually less used
-and bigger. In order to not affect the overall bundle size of the Argon Kit, they should be imported locally.
+and bigger. In order to not affect the overall bundle size of the White Kit, they should be imported locally.
 
 Here's the list of local components:
 
--- vue-white-dashboard/src/layout/dashboard/DashboardLayout.vue
+++ vue-white-dashboard/src/layout/dashboard/DashboardLayout.vue
@@ -2,27 +2,84 @@
   <div class="wrapper">
     <side-bar :background-color="backgroundColor">
       <template slot="links">
-        <sidebar-link to="/dashboard" :name="$t('sidebar.dashboard')" icon="tim-icons icon-chart-pie-36"/>
-        <sidebar-link to="#" :name="$t('sidebar.examples')" icon='fab fa-vuejs fa-2x'/>
-        <sidebar-link to="/examples/user-profile" :name="$t('sidebar.userProfile')" class="ml-5"/>
-        <sidebar-link to="/examples/user-management/list-users" :name="$t('sidebar.userManagement')" class="ml-5" />
-        <sidebar-link to="/icons" :name="$t('sidebar.icons')" icon="tim-icons icon-atom"/>
-        <sidebar-link to="/maps" :name="$t('sidebar.maps')" icon="tim-icons icon-pin"/>
-        <sidebar-link to="/notifications" :name="$t('sidebar.notifications')" icon="tim-icons icon-bell-55"/>
-        <sidebar-link to="/profile" :name="$t('sidebar.userProfile')" icon="tim-icons icon-single-02"/>
-        <sidebar-link to="/table-list" :name="$t('sidebar.tableList')" icon="tim-icons icon-puzzle-10"/>
-        <sidebar-link to="/typography" :name="$t('sidebar.typography')" icon="tim-icons icon-align-center"/>
-        <sidebar-link to="/dashboard?enableRTL=true" :name="$t('sidebar.rtlSupport')" icon="tim-icons icon-world"/>
+        <sidebar-link
+          to="/dashboard"
+          :name="$t('sidebar.dashboard')"
+          icon="tim-icons icon-chart-pie-36"
+        />
+        <sidebar-link
+          to="#"
+          :name="$t('sidebar.examples')"
+          icon="fab fa-vuejs fa-2x"
+        />
+        <sidebar-link
+          to="/examples/user-profile"
+          :name="$t('sidebar.userProfile')"
+          class="ml-5"
+        />
+        <sidebar-link
+          to="/examples/user-management/list-users"
+          :name="$t('sidebar.userManagement')"
+          class="ml-5"
+        />
+        <sidebar-link
+          to="/icons"
+          :name="$t('sidebar.icons')"
+          icon="tim-icons icon-atom"
+        />
+        <sidebar-link
+          to="/maps"
+          :name="$t('sidebar.maps')"
+          icon="tim-icons icon-pin"
+        />
+        <sidebar-link
+          to="/notifications"
+          :name="$t('sidebar.notifications')"
+          icon="tim-icons icon-bell-55"
+        />
+        <sidebar-link
+          to="/profile"
+          :name="$t('sidebar.userProfile')"
+          icon="tim-icons icon-single-02"
+        />
+        <sidebar-link
+          to="/table-list"
+          :name="$t('sidebar.tableList')"
+          icon="tim-icons icon-puzzle-10"
+        />
+        <sidebar-link
+          to="/typography"
+          :name="$t('sidebar.typography')"
+          icon="tim-icons icon-align-center"
+        />
+        <sidebar-link
+          to="/dashboard?enableRTL=true"
+          :name="$t('sidebar.rtlSupport')"
+          icon="tim-icons icon-world"
+        />
+        <a
+          href="https://www.creative-tim.com/product/vue-white-dashboard-pro-laravel"
+          target="_blank"
+          class="btn btn-warning"
+          rel="noopener"
+          style="margin-left: 13px"
+        >
+          <span class="btn-inner--icon" style="margin-left: -26px"
+            ><i class="fas fa-download mr-2"></i
+          ></span>
+          <span
+            class="nav-link-inner--text"
+            style="margin-left: -34px; margin-right: 25px; margin-top: 5px"
+            >Upgrade to PRO</span
+          ></a
+        >
       </template>
     </side-bar>
-    <sidebar-share :background-color.sync="backgroundColor">
-    </sidebar-share>
+    <sidebar-share :background-color.sync="backgroundColor"> </sidebar-share>
     <div class="main-panel" :data="backgroundColor">
       <top-navbar></top-navbar>
 
-      <dashboard-content @click.native="toggleSidebar">
-
-      </dashboard-content>
+      <dashboard-content @click.native="toggleSidebar"> </dashboard-content>
 
       <content-footer></content-footer>
     </div>
@@ -42,19 +99,19 @@ export default {
     ContentFooter,
     DashboardContent,
     MobileMenu,
-    SidebarShare
+    SidebarShare,
   },
   data() {
     return {
-      backgroundColor: "primary"
-    }
+      backgroundColor: "primary",
+    };
   },
   methods: {
     toggleSidebar() {
       if (this.$sidebar.showSidebar) {
         this.$sidebar.displaySidebar(false);
       }
-    }
-  }
+    },
+  },
 };
 </script>
